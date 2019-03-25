3f7190dc42543e982676a4e1e3d17836b359b30c
Raymun Kohli <raymun1@hotmail.co.uk>
2019-03-25 16:39:51 +0000

    123

--- cloudmusic/static/js/googlejs.js
+++ cloudmusic/static/js/googlejs.js
@@ -1,3 +1,21 @@
+// using jQuery
+function getCookie(name) {
+  var cookieValue = null;
+  if (document.cookie && document.cookie !== '') {
+      var cookies = document.cookie.split(';');
+      for (var i = 0; i < cookies.length; i++) {
+          var cookie = jQuery.trim(cookies[i]);
+          // Does this cookie string begin with the name we want?
+          if (cookie.substring(0, name.length + 1) === (name + '=')) {
+              cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+              break;
+          }
+      }
+  }
+  return cookieValue;
+}
+var csrftoken = getCookie('csrftoken');
+
 function onSignIn(googleUser) {
   var profile = googleUser.getBasicProfile();
   console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
@@ -5,11 +23,10 @@ function onSignIn(googleUser) {
   console.log('Image URL: ' + profile.getImageUrl());
   console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
   var xhr = new XMLHttpRequest();
-  var id_token = googleUser.getAuthResponse().id_token;
-  xhr.open('POST', 'localhost:1116/loggedin');
-  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-  xhr.onload = function() {
-    console.log('Signed in as: ' + xhr.responseText);
-  };
-  xhr.send('idtoken=' + id_token);
+  xhr.open("POST", "https://directed-sonar-234413.appspot.com/loggedin", true);
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  xhr.setRequestHeader('X-CSRFToken', csrftoken);
+  xhr.send(JSON.stringify({
+    Email: profile.getEmail()
+  }));
 }
